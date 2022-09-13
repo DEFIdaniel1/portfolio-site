0ee57f9737803b542d072a4246d5bf45ea90d1ac
danala <greatprotectionwallet@gmail.com>
2022-09-13 17:40:59 -0600

    header added with video and masking

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,26 @@
 import React from 'react'
-import './Header.module.scss'
+import classes from './Header.module.scss'
+
+import headerImg from '../images/3dblue.jpg'
+import wordLights from '../images/wordlights.jpg'
+import puddle from '../images/puddle.jpg'
+import purpleLines from '../images/purpleLines.jpg'
+import neonBuilding from '../images/neonBuilding.jpg'
+import headerVid from '../video/inkdrops.mp4'
+import headerVid2 from '../video/inkswipe.mp4'
+import headerVid3 from '../video/inkdrops2.mp4'
 
 const Header = () => {
     return (
-        <div className="container">
-            <div>Some Text</div>
-            <img src="" alt="" />
+        <div className={classes.container}>
+            <div className={classes.textbox}>
+                <h1>I'm Daniel.</h1>
+                <h2>A Web Developer.</h2>
+                <h2>A Blockhain Developer.</h2>
+                <h2>And UI Obsessed...</h2>
+            </div>
+            <img src={neonBuilding} alt="" />
+            <video src={headerVid} type="video/mp4" autoPlay muted loop></video>
         </div>
     )
 }

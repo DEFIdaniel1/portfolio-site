9476b6f32a3a30803e9a8c78926f0b7f812e531f
danala <greatprotectionwallet@gmail.com>
2022-09-13 15:07:55 -0600

    initialize project

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.module.scss'
+
+const Header = () => {
+    return (
+        <div className="container">
+            <div>Some Text</div>
+            <img src="" alt="" />
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+// import { NavLink } from 'react-router-dom'
+import './Navbar.scss'
+
+function Navbar({ account, connectWallet, disconnectWallet }) {
+    const [openNav, setOpenNav] = useState(false)
+    const openNavHandler = () => {
+        setOpenNav(!openNav)
+    }
+
+    return (
+        <div className={!openNav ? 'navbar navbar__closed' : 'navbar navbar__open'}>
+            <div
+                className={!openNav ? 'navBurger navBurger__closed' : 'navBurger navBurger__open'}
+                onClick={openNavHandler}
+            >
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <nav className={!openNav ? 'nav nav__closed' : 'nav nav__open'}>
+                <div className="nav__item" onClick={openNavHandler}>
+                    About Me
+                </div>
+                <div className="nav__item" onClick={openNavHandler}>
+                    Skills
+                </div>
+                <div className="nav__item" onClick={openNavHandler}>
+                    Portfolio
+                </div>
+                <div className="nav__item" onClick={openNavHandler}>
+                    Experience
+                </div>
+            </nav>
+        </div>
+    )
+}
+export default Navbar

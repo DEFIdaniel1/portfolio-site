30b88be2ffea57dff60f00a22dba0718b413479a
danala <greatprotectionwallet@gmail.com>
2022-09-16 16:08:53 -0600

    portfolio update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -8,8 +8,13 @@ const Portfolio = () => {
             title: 'GAL-XY',
             subtitle: 'NFT Marketplace',
             description:
-                'Some description. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Explicabo earum iusto vitae, iure modi sint! Porro illum perferendis ipsum Voluptate similique assumenda exercitationem sapiente iusto facilis molestias sunt veritatis praesentium?',
-            features: ['Something', 'Something else', 'Another thing'],
+                'A website built from the ground-up. Custom solidity smart contracts for minting and listing NFTs on the platform. A random NFT mint feature that incorporates user inputs.',
+            features: [
+                'Solidity smart contracts for NFT minting and NFT marketplace',
+                'Fully customized UI using Javascript and CSS',
+                'Project is live on Goerli testnet',
+                'React project with ethers.js blockchain connections',
+            ],
             github: 'www',
             link: 'www',
             gif: 'something',
